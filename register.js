e1d342b891a05d39b4caefa3a598164608b6d09f
kuzuri1709 <ctcxre@gmail.com>
2021-04-11 16:05:12 +0700

    adfa

--- register.js
+++ register.js
@@ -1,6 +1,15 @@
-var email = document.getElementById("email").value;
-var username = document.getElementById("username").value;
-var password = document.getElementById("pwd").value;
-function getUserInfo(email, password, username) {
-    if(email != null)
+var email 
+var username 
+var password 
+var submit = document.getElementById("submit-btn");
+function User(email, password, username){
+    this.email = email
+    this.password = password
+    this.username = username
+}
+function getUserInfo(){
+    email = document.getElementById("email").value;
+    username = document.getElementById("username").value;
+    password = document.getElementById("pwd").value;
+    return user = new User(email, password, username)
 }
