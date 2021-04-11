33728f083802fa79d0c54cf09b0c8c30431b5414
kuzuri1709 <ctcxre@gmail.com>
2021-04-11 15:20:12 +0700

    Update register.js

---
+++ register.js
@@ -0,0 +1,6 @@
+var email = document.getElementById("email").value;
+var username = document.getElementById("username").value;
+var password = document.getElementById("pwd").value;
+function getUserInfo(email, password, username) {
+    if(email != null)
+}
