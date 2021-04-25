1337f3aad5b2207b864626f553350ff5e915f7cc
kuzuri1709 <ctcxre@gmail.com>
2021-04-25 13:12:34 +0700

    gg

--- register.js
+++ register.js
@@ -1,15 +1,4 @@
-var email 
-var username 
-var password 
-var submit = document.getElementById("submit-btn");
-function User(email, password, username){
-    this.email = email
-    this.password = password
-    this.username = username
-}
-function getUserInfo(){
-    email = document.getElementById("email").value;
-    username = document.getElementById("username").value;
-    password = document.getElementById("pwd").value;
-    return user = new User(email, password, username)
-}
+var create = document.getElementById("create")
+        create.addEventListener("click", function () {
+            sendUserInfo()
+        })
