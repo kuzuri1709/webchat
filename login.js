dcf76f15c3efef334d54797b7fb30a2e9a2823b1
kuzuri1709 <ctcxre@gmail.com>
2021-05-31 23:39:04 +0700

    bitch =)))

--- login.js
+++ login.js
@@ -1,12 +1,1 @@
-let btnSignIn = document.getElementById("Sign");
-btnSignIn.addEventListener("click", function(){
-    let username = document.getElementById("username").value;
-    let pwd = document.getElementById("pwd").value;
-
-    
-});
-
-function getData(snapshot){
-    firebase.database().ref("users")
-}
 
