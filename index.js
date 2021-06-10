c843db3be62ad22801ef3dffcb15387f56e98997
kuzuri1709 <ctcxre@gmail.com>
2021-06-10 10:55:47 +0700

    hey bitch

---
+++ index.js
@@ -0,0 +1,41 @@
+var myName = prompt("Enter your name");
+function sendMessage() {
+
+    //get message
+    var message = document.getElementById("message").value;
+
+    //save in database
+    firebase.database().ref("messages").push().set({
+        "sender": myName,
+        "message": message,
+    });
+
+    //prevent form from submitting
+    return false;
+}
+
+//listen for incoming messages
+firebase.database().ref("messages").on("child_added", function (snapshot) {
+    var html = `<li class="messageBox"> 
+      ${snapshot.val().sender}:  ${snapshot.val().message}
+      </li>`
+
+    document.getElementById("messages").innerHTML += html;
+    document.getElementById("message").value = "";
+    updateScroll();
+});
+
+
+
+var scrolled = false;
+function updateScroll() {
+    if (!scrolled) {
+        var element = document.getElementById("messages");
+        element.scrollTop = element.scrollHeight;
+    }
+    console.log("hahaha");
+}
+
+document.getElementById("messages").addEventListener("scroll", function () {
+    sccrolled = true;
+})
---
+++ register.js
@@ -0,0 +1,46 @@
+// function sendUserInfo() {
+//     function User(email, password, username) {
+//         this.email = email
+//         this.password = password
+//         this.username = username
+//     }
+//     var user
+
+//     getUserInfo()
+//     function checkPwd(pwInput, password) {
+//         //Check password
+
+//         if (password.length >= 8) {
+//             //valid
+//             console.log("valid password")
+//             return true;
+//         } else {
+//             //invalid
+//             console.log('invalid password')
+//         }
+//     }
+
+//     //save in database
+//     firebase.database().ref("users").push().set({
+//         "username": user.username,
+//         "email": user.email,
+//         "password": user.password,
+//     });
+
+//     //prevent form from submitting
+//     return false;
+// }
+
+// var btnCreate = document.getElementById("create-button");
+// btnCreate.addEventListener("click", function () {
+//     sendUserInfo();
+// })
+
+//getting data from database
+db.collection('users').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data());
+    })
+});
+
+//saving data
