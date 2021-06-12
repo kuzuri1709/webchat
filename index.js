278b0e338b3d103170196708192a335bc711832e
kuzuri1709 <ctcxre@gmail.com>
2021-06-12 11:43:49 +0700

    index

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var retrievedUser = JSON.parse(localStorage.getItem("user"))
+var retrievedUser = JSON.parse(localStorage.getItem("user"));
+var retrievedId = localStorage.getItem("userId");
 // console.log(retrievedUser);
+// console.log(localStorage.getItem("user"));
+// console.log(localStorage.getItem("userId"));
+
 localStorage.removeItem("user");
+localStorage.removeItem("userId");
 
 var myName = retrievedUser.username;
-for (let i = 0; i = 1;) {
-    if (myName == "") {
-        myName = prompt("Enter your name");
-    } else {
-        i++;
-        break;
-    }
-}
+// for (let i = 0; i = 1;) {
+//     if (myName == "") {
+//         myName = prompt("Enter your name");
+//     } else {
+//         i++;
+//         break;
+//     }
+// }
 
 
 function sendMessage() {
@@ -36,27 +41,34 @@ function sendMessage() {
 }
 
 //listen for incoming messages
-firebase.database().ref("messages").on("child_added", function (snapshot) {
-    var html = `<div class = "chatRow"><p class="messageBox"> 
-      ${snapshot.val().sender}:  ${snapshot.val().message}
-      </p> <span class = "time">${snapshot.val().time}</span></div>`
+// firebase.database().ref("messages").on("child_added", function (snapshot) {
+//     var html = `<div class = "chatRow"><p class="messageBox"> 
+//       ${snapshot.val().sender}:  ${snapshot.val().message}
+//       </p> <span class = "time">${snapshot.val().time}</span></div>`
 
-    document.getElementById("messages").innerHTML += html;
-    document.getElementById("message").value = "";
-    // updateScroll();
-});
+//     document.getElementById("messages").innerHTML += html;
+//     document.getElementById("message").value = "";
+//     // updateScroll();
+// });
 
 var chat_room_id;
 function sendMessageWithId(chat_room_id, message) {
-    let today = new Date();
+    
+    var message = document.getElementById("message").value;
+    var today = new Date();
+    var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
 
     if (message != "") {
         firebase.database().ref("ChatRoom/" + chat_room_id).push().set({
-            "sender": "hiep_tv",
+            "sender": myName,
             "message": message,
-            "time": today.getTime()
+            "time": dateTime
         });
     }
+
+    return false;
 }
 
 var usersList = document.getElementById("users_field");
@@ -69,10 +81,21 @@ firebase.database().ref("users").on("child_added", function (snapshot) {
     var list = document.getElementsByClassName("user_box");
     for (let i = 0; i < list.length; i++) {
         list[i].addEventListener('click', () => {
-            chat_room_id = list[i].id + "a";
-            console.log(chat_room_id);
+            chat_room_id = list[i].id + retrievedId;
+            // console.log(chat_room_id);
+            // sendMessageWithId(chat_room_id, message);
         })
     }
+});
+
+firebase.database().ref("ChatRoom/" + chat_room_id).on("child_added", function (snapshot) {
+    var html = `<div class = "chatRow"><p class="messageBox"> 
+      ${snapshot.val().sender}:  ${snapshot.val().message}
+      </p> <span class = "time">${snapshot.val().time}</span></div>`
+
+    document.getElementById("messages").innerHTML += html;
+    document.getElementById("message").value = "";
+    // updateScroll();
 });
 // {
 //     id,
